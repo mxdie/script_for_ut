0a9beeb11b8b8209d527255c70d4cda637d31fc2
leurd <leurd@qq..com>
2019-05-13 14:19:09 +0800

    debug

--- autoseed_0day/test.py
+++ autoseed_0day/test.py
@@ -5,51 +5,44 @@ from time import sleep
 try:
         with open('config.json','r') as f:
                 config = json.load(f)
-                cookies = config['web']['cookies']
-                passkey = config['web']['passkey']
-
-        # 判断cookie
-        print('少女祈祷中...')
-        print('')
-        headers= {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
-                  'cookie':cookies}
-        try:
-                html_request=request.Request(url='https://nanyangpt.com/index.php',headers=headers)
-                html_text=request.urlopen(html_request).read().decode('utf-8')
-                if html_text.find('登录')==-1:
-                        print('cookies可以用。')
-                else:
-                        print('cookies不能用。')
-
-        except:
-                print('无法访问网站检查cookies，请确认网络连接,如果可以正常访问，请前往论坛报错。')
-        sleep(1)
-        print('')
-        print('少女继续祈祷中...')
-        print('')
-        opener = request.build_opener()
-        opener.addheaders = [('User-agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36')]
-        request.install_opener(opener)
-        zz_url1='https://nanyangpt.com/download.php?id='
-        zz_url2='&passkey='
-        zz_url=zz_url1+'19839'+zz_url2+passkey
-        list_zz=[]
-        def  huizhi(a,b,c):
-                list_zz.append(a)
-        A=request.urlretrieve(zz_url,'temp.torrent',huizhi)
-        if len(list_zz) >=3:
-                print('passkey可以用。')
-        else:
-                print('passkey有问题，或者网络有问题，确认没问题的话来论坛报错。')
-        print('')
-        sleep(1)
-        print('少女祈祷完毕。')
-        print('')
-        os.remove('temp.torrent')
-
+        cookie = config['nanyang']['cookie']
 except:
-        print('找不到配置文件，或者有权限问题。')
-sys.exit()
+        print('error: config')
+
+# 判断cookie
+print('少女祈祷中...')
+print('')
+opener = request.build_opener()
+opener.addheaders = [('User-agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36'),
+                ('cookie',cookie)]
+request.install_opener(opener)
+try:
+        html_text=request.urlopen(url='https://nanyangpt.com/index.php').read().decode('utf-8')
+        if html_text.find('登录')==-1:
+                print('cookies可以用。')
+        else:
+                print('cookies不能用。')
+except:
+        print('无法访问网站检查cookies，请确认网络连接')
+sleep(1)
+print('')
+print('少女继续祈祷中...')
+print('')
+
+zz_url='https://nanyangpt.com/download.php?id=69470'
+list_zz=[]
+def  huizhi(a,b,c):
+        list_zz.append(a)
+A=request.urlretrieve(zz_url,'temp.torrent',huizhi)
+if len(list_zz) >=3:
+        print('passkey可以用。')
+else:
+        print('passkey有问题，或者网络有问题，确认没问题的话来论坛报错。')
+print('')
+sleep(1)
+print('少女祈祷完毕。')
+print('')
+os.remove('temp.torrent')
 print('10秒 后')
 sleep(1)
 print('9秒  自')
